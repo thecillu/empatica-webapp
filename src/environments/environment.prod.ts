61cd24f5d05ff4dae7b71a77cfbc300e13b3aeb4
Filippo Cilluffo <filippo.cilluffo@nexi.it>
2019-12-27 17:46:33 +0100

    changed endpoint to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://empatica-load-balancer-1904015388.eu-central-1.elb.amazonaws.com/articles'
+  apiBaseUrl: 'https://empatica-load-balancer-1904015388.eu-central-1.elb.amazonaws.com/articles'
 };
